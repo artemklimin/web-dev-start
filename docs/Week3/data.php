ac1b97f1563770b575e297c6e19988763640b008
AVKlimin <avklimin@yandex.ru>
2017-11-28 15:40:17 +0300

    Карта с буллетом

--- docs/Week3/data.php
+++ docs/Week3/data.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-   if (isset($_POST['name']) && isset($_POST['phone'])isset($_POST['street']) && isset($_POST['house']) && isset($_POST['corp']) && isset($_POST['flat']) && isset($_POST['flor']) && isset($_POST['comment']) && isset($_POST['change']) && isset($_POST['pay-card']) && isset($_POST['no-call'])) {
+   if (isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['street']) && isset($_POST['house']) && isset($_POST['corp']) && isset($_POST['flat']) && isset($_POST['flor']) && isset($_POST['comment']) && isset($_POST['change']) && isset($_POST['pay-card']) && isset($_POST['no-call'])) {
       echo 'Данные получены. Имя: '.$_POST['name'].' Возраст: '.$_POST['phone'];
    }
    else {
